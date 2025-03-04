30a87a02192bbd49b832a7e880d96b7972a77436
Joaquin <joaquingamboa@hotmail.com>
2025-03-04 17:29:10 -0500

    optimizando los cambios

--- modules/Sale/Resources/views/sale_opportunities/form.blade.php
+++ modules/Sale/Resources/views/sale_opportunities/form.blade.php
@@ -1,10 +1,6 @@
 @extends('tenant.layouts.app')
 
 @section('content')
-    @if(session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
+
     <tenant-sale-opportunities-form :type-user="{{json_encode(Auth::user()->type)}}" :id="{{json_encode($id)}}"></tenant-sale-opportunities-form>
 @endsection
